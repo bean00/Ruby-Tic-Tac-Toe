a98b89046d057a4831f9e6c0bfae8f632713d085
Jon Chin <jchin@8thlight.com>
2017-08-21 12:49:47 -0700

    Add 'moves to position 1' test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,12 @@
 require 'board'
 
-
+describe 'move' do
+  it 'moves to position 1' do
+    move_at_1 = [
+      ["x", "", ""],
+      ["",  "", ""],
+      ["",  "", ""]
+    ]
+    expect(Board.move(1)).to eq(move_at_1)
+  end
+end
