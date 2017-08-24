97344af7facad96c58f3abdcead944952469f1bf
Jon Chin <jchin@8thlight.com>
2017-08-24 15:15:52 -0700

    Get the rest of the has_player_won tests to pass in score_tracker_spec

--- spec/score_tracker_spec.rb
+++ spec/score_tracker_spec.rb
@@ -7,7 +7,6 @@ describe 'has_player_won' do
     board.move("tl", "x")
     board.move("t", "x")
     board.move("tr", "x")
-
     st = ScoreTracker.new(board.board)
 
     expect(st.has_player_won("x")).to be true
@@ -17,6 +16,24 @@ describe 'has_player_won' do
     board = Board.new
     st = ScoreTracker.new(board.board)
     expect(st.has_player_won("x")).to be false
+  end
+
+  it 'returns true when the middle row is filled' do
+    board = Board.new
+    board.move("l", "o")
+    board.move("c", "o")
+    board.move("r", "o")
+    st = ScoreTracker.new(board.board)
+    expect(st.has_player_won("o")).to be true
+  end
+
+  it 'returns true when the bottom row is filled' do
+    board = Board.new
+    board.move("bl", "x")
+    board.move("b", "x")
+    board.move("br", "x")
+    st = ScoreTracker.new(board.board)
+    expect(st.has_player_won("x")).to be true
   end
 end
 
