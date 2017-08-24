3d9ee3062ffc562e0f2eca6490736ac97a1bb467
Jon Chin <jchin@8thlight.com>
2017-08-24 15:31:01 -0700

    Finish refactoring has_player_won methods, moving from board_spec to score_tracker_spec

--- spec/score_tracker_spec.rb
+++ spec/score_tracker_spec.rb
@@ -37,3 +37,74 @@ describe 'has_player_won' do
   end
 end
 
+
+describe 'extract_rows' do
+  it 'returns all rows for a board with 3 moves' do
+    board = Board.new
+    board.move("tl", "x")
+    board.move("c", "o")
+    board.move("bl", "x")
+    st = ScoreTracker.new(board.board)
+
+    rows = [["x", "",  ""],
+            ["",  "o", ""],
+            ["x", "",  ""]]
+
+    expect(st.extract_rows).to eq rows
+  end
+
+  it 'returns all rows for an empty board' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    rows = [["", "", ""],
+            ["", "", ""],
+            ["", "", ""]]
+    expect(st.extract_rows).to eq rows
+  end
+end
+
+
+describe 'are_any_rows_full' do
+  it 'returns true if one row is full' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    rows = [["x", "x", "x"],
+            ["",  "",  ""],
+            ["",  "",  ""]]
+    expect(st.are_any_rows_full(rows, "x")).to be true
+  end
+
+  it 'returns false if no row is full' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    rows = [["x", "",  ""],
+            ["",  "",  ""],
+            ["x", "x", "o"]]
+    expect(st.are_any_rows_full(rows, "x")).to be false
+  end
+end
+
+
+describe 'is_row_full' do
+  it 'returns true if the row is full' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    row = ["x", "x", "x"]
+    expect(st.is_row_full(row, "x")).to be true
+  end
+
+  it 'returns false if the row is empty' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    row = ["", "", ""]
+    expect(st.is_row_full(row, "x")).to be false
+  end
+
+  it 'returns false if the row has mixed characters' do
+    board = Board.new
+    st = ScoreTracker.new(board.board)
+    row = ["x", "o", "x"]
+    expect(st.is_row_full(row, "x")).to be false
+  end
+end
+
