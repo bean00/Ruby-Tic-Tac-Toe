c52432c3290a4b85f34be154a459d6db8708f799
Jon Chin <jchin@8thlight.com>
2017-10-12 10:01:26 -0700

    Use Minimax in ComputerPlayer

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,55 +1,22 @@
+require_relative 'minimax'
 require_relative 'player'
 require 'deep_clone'
 
 class ComputerPlayer < Player
 
   def initialize(token, other_token, score_tracker)
+    @minimax = Minimax.new(token, other_token, score_tracker)
     @token = token
-    @other_token = other_token
-    @score_tracker = score_tracker
   end
 
   def get_next_move(board)
-    move = minimax_score(board, @token)[:move]
+    move = @minimax.minimax_move_and_score(board, @token)[:move]
 
     move.to_s
   end
 
   def get_token
     @token
-  end
-
-  private
-
-  def minimax_score(board, next_player_token)
-    previous_player_token = (next_player_token == @token) ? @other_token : @token
-
-    @score_tracker.update_scores(previous_player_token, board)
-
-    current_score = @score_tracker.get_player_score(@token)
-
-    if @score_tracker.is_game_finished?
-      return { move: 0, score: current_score }
-    else
-      possible_moves = board.get_available_moves
-      
-      moves_and_scores = Hash.new
-
-      possible_moves.each_with_index { |move, i| 
-        cloned_board = DeepClone.clone(board)
-        cloned_board.move(move, next_player_token)
-
-        score = minimax_score(cloned_board, previous_player_token)[:score]
-        moves_and_scores[move] = score
-      }
-
-      scores = moves_and_scores.values
-      score = (next_player_token == @token) ? scores.max : scores.min
-
-      next_move = moves_and_scores.key(score)
-
-      return { move: next_move, score: score }
-    end
   end
 
 end
